54f7791421ea2c128ef5b6d57ad34f81ffd255a9
47 PC1 <sinkvadev@gmail.com>
2024-12-21 03:36:44 +0700

    first settings

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,6 @@
+head: {
+    defaults: {
+        title: 'Gorm',
+        useSocialMetaTags: true
+    }
+}
---
+++ tars-config.js
@@ -0,0 +1,64 @@
+module.exports = {
+    "postcss": [],
+    "svg": {
+        "active": true,
+        "workflow": "sprite",
+        "symbolsConfig": {
+            "loadingType": "inject",
+            "usePolyfillForExternalSymbols": true,
+            "pathToExternalSymbolsFile": ""
+        }
+    },
+    "css": {
+        "workflow": "manual"
+    },
+    "js": {
+        "workflow": "concat",
+        "bundler": "webpack",
+        "lint": true,
+        "useBabel": true,
+        "removeConsoleLog": true,
+        "webpack": {
+            "useHMR": false,
+            "providePlugin": {}
+        },
+        "jsPathsToConcatBeforeModulesJs": [],
+        "lintJsCodeBeforeModules": false,
+        "jsPathsToConcatAfterModulesJs": [],
+        "lintJsCodeAfterModules": false
+    },
+    "sourcemaps": {
+        "js": {
+            "active": true,
+            "inline": true
+        },
+        "css": {
+            "active": true,
+            "inline": true
+        }
+    },
+    "notifyConfig": {
+        "useNotify": false,
+        "title": "TARS notification",
+        "sounds": {},
+        "taskFinishedText": "Task finished at: "
+    },
+    "minifyHtml": false,
+    "generateStaticPath": true,
+    "devPath": "./dev/",
+    "buildPath": "./builds/",
+    "useBuildVersioning": true,
+    "useArchiver": true,
+    "ulimit": 4096,
+    "templater": "handlebars",
+    "cssPreprocessor": "scss",
+    "useImagesForDisplayWithDpi": [
+        96
+    ],
+    "fs": {
+        "staticFolderName": "public",
+        "imagesFolderName": "img",
+        "componentsFolderName": "components"
+    },
+    "staticPrefix": "public/"
+};
